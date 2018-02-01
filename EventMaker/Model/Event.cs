4a283b53764e16f41b12278b16ef9e200ce15b56
Patrik Ando <patr589d@edu.easj.dk>
2018-02-01 10:28:50 +0100

    ADDED : EventCatalogSingleton & Event

---
+++ EventMaker/Model/Event.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Globalization.DateTimeFormatting;
+
+namespace EventMaker.Model
+{
+    class Event
+    {
+        private DateTime _dateTime;
+        private int _id;
+        private string _description;
+        private string _name;
+        private string _place;
+
+        public Event()
+        {
+            
+        }
+
+        public DateTime DateTime { get => _dateTime; set => _dateTime = value; }
+        public global::System.Int32 Id { get => _id; set => _id = value; }
+        public global::System.String Description { get => _description; set => _description = value; }
+        public global::System.String Name { get => _name; set => _name = value; }
+        public global::System.String Place { get => _place; set => _place = value; }
+    }
+}
---
+++ EventMaker/Model/EventCatalogSingleton.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMaker.Model
+{
+    class EventCatalogSingleton
+    {
+        private ObserverableCollection<Event> _events;
+        private static EventCatalogSingleton Instance { get , set; }
+
+        private EventCatalogSingleton()
+        {
+            _events = new ObserverableCollection<Event>();
+        }
+
+        public static EventCatalogSingleton GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new EventCatalogSingleton();
+            }
+
+            return Instance;
+        }
+
+        public ObserverableCollection<Event> SetEvents(ObserverableCollection<Event> events)
+        {
+            _events = events;
+        }
+
+        public ObserverableCollection<Event> GetEvent()
+        {
+            return _events;
+        }
+
+        #region Operations
+
+        //public void AddEVent(Event Event)
+        //{
+
+        //}
+
+        //public void async LoadEventsAsync()
+        //{
+
+        //}
+
+        //public void RemoveEvent(Event Event)
+        //{
+
+        //}
+
+        #endregion
+    }
+}
